7657937361ed94fdd47f56d3d12ff62c8ee6e9e9
unknown <cclaudio@uwo.ca>
2018-03-05 15:25:37 -0500

    Add RPN functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Declare and initialize variables
+// Declare and initialize variables with deafult values
 var num = 0;
 var ans = 0;
 var opr = ' ';
@@ -11,16 +11,16 @@ function calculate(key) {
     // state00: Calculates ans
     case 0:
       if (key == ' ' || key == '-' || key == '+' || key == '*' || key == '/') {
-        opr = key;
+        if(mode == "INFIX") {
+          opr = key;
+        }
       }
       else if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9') {
-        document.getElementById("txtbx").value = key;
-        num = document.getElementById("txtbx").value;
+        updateDisplay(key);
         state = 1;
       }
       else if (key == '.') {
-        document.getElementById("txtbx").value = "0.";
-        num = document.getElementById("txtbx").value;
+        updateDisplay("0.");
         state = 2;
       }
       break;
@@ -29,16 +29,30 @@ function calculate(key) {
     // state01: Displaying and accumulating num
     case 1:
       if (key == ' ' || key == '-' || key == '+' || key == '*' || key == '/') {
-        document.getElementById("txtbx").value = calculateAns();
-        opr = key;
-        state = 0;
+        if (mode == "RPN") {
+          if (document.getElementById("ENTER").disabled == true) {
+            opr = key;
+            document.getElementById("txtbx").value = calculateAns();
+            state = 0;
+          }
+        }
+        else if (mode == "INFIX") {
+          document.getElementById("txtbx").value = calculateAns();
+          opr = key;
+          state = 0;
+        }
       }
       else if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9' || key == '.') {
-        document.getElementById("txtbx").value += key;
-        num = document.getElementById("txtbx").value;
+        updateDisplay(num + key);
         if (key == '.') {
           state = 2;
         }
+      }
+      else if (key == "ENTER") {
+        alert("ENTER HAS BEEN PRESSED");
+        ans = num;
+        document.getElementById("ENTER").disabled = true;
+        state = 3;
       }
       break;
 
@@ -50,8 +64,19 @@ function calculate(key) {
         state = 0;
       }
       else if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9') {
-        document.getElementById("txtbx").value += key;
-        num = document.getElementById("txtbx").value;
+        updateDisplay(num + key);
+      }
+      break;
+
+      // state03: This state is only used when in RPN mode
+    case 3:
+      if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9') {
+        updateDisplay(key);
+        state = 1;
+      }
+      else if (key == '.') {
+        updateDisplay("0.");
+        state = 2;
       }
       break;
 
@@ -87,24 +112,33 @@ function calculateAns() {
     return ans;
 }
 
+function updateDisplay(disp_num) {
+  document.getElementById("txtbx").value = disp_num;
+  num = document.getElementById("txtbx").value;
+}
+
 
 function switchMode(x) {
   if (mode != x) {
     document.getElementById("INFIX").disabled = false;
     document.getElementById("RPN").disabled = false;
     document.getElementById(x).disabled = true;
-    mode = x;
 
+    mode = x;
     console.log("Changed mode to " + x);
 
     // Reset calculator values
+    if(mode == "RPN") {
+      document.getElementById("ENTER").disabled = false;
+      state = 1;
+    }
+    else {
+      document.getElementById("ENTER").disabled = true;
+      state = 0;
+    }
     num = 0;
     ans = 0;
     opr = ' ';
-    state = 0;
-
-    calculate(' ');
-    // Delete later once RPN is implemented
     document.getElementById("txtbx").value = ans;
   }
 }
