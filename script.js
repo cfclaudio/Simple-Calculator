e192c6957aa3e605e1034a69b1ad845226eceb99
unknown <cclaudio@uwo.ca>
2018-03-05 10:44:57 -0500

    Add decimal functionality

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ var opr = ' ';
 var state = 0;
 var mode = "INFIX";
 
-function reset() {
-  var num = 0;
-  var ans = 0;
-  var opr = ' ';
-  var state = 0;
-}
 
 function calculate(key) {
   switch(state) {
@@ -21,8 +15,13 @@ function calculate(key) {
       }
       else if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9') {
         document.getElementById("txtbx").value = key;
-        num = key;
+        num = document.getElementById("txtbx").value;
         state = 1;
+      }
+      else if (key == '.') {
+        document.getElementById("txtbx").value = "0.";
+        num = document.getElementById("txtbx").value;
+        state = 2;
       }
       break;
 
@@ -34,11 +33,28 @@ function calculate(key) {
         opr = key;
         state = 0;
       }
+      else if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9' || key == '.') {
+        document.getElementById("txtbx").value += key;
+        num = document.getElementById("txtbx").value;
+        if (key == '.') {
+          state = 2;
+        }
+      }
+      break;
+
+    // state02: Displays and accumulates digits for decimal case
+    case 2:
+      if (key == ' ' || key == '-' || key == '+' || key == '*' || key == '/') {
+        document.getElementById("txtbx").value = calculateAns();
+        opr = key;
+        state = 0;
+      }
       else if (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9') {
         document.getElementById("txtbx").value += key;
         num = document.getElementById("txtbx").value;
       }
       break;
+
   }
 
 
@@ -57,16 +73,16 @@ function calculateAns() {
       ans = num;
     }
     else if (opr == '-') {
-      ans = ans - num;
+      ans = parseFloat(ans) - parseFloat(num);
     }
     else if (opr == '+') {
-      ans = Number(ans) + Number(num);
+      ans = parseFloat(ans) + parseFloat(num);
     }
     else if (opr == '*') {
-      ans = ans * num;
+      ans = parseFloat(ans) * parseFloat(num);
     }
     else if (opr == '/') {
-      ans = ans / num;
+      ans = parseFloat(ans) / parseFloat(num);
     }
     return ans;
 }
@@ -78,5 +94,17 @@ function switchMode(x) {
     document.getElementById("RPN").disabled = false;
     document.getElementById(x).disabled = true;
     mode = x;
+
+    console.log("Changed mode to " + x);
+
+    // Reset calculator values
+    num = 0;
+    ans = 0;
+    opr = ' ';
+    state = 0;
+
+    calculate(' ');
+    // Delete later once RPN is implemented
+    document.getElementById("txtbx").value = ans;
   }
 }
